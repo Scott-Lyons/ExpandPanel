57224afd634be1304229e00f88ef435a669cd5c9
ESENDEX\scott.lyons <scott.lyons@esendex.com>
2017-07-03 11:19:22 +0100

    Reformatted url get method

--- src/ExpandPanel/Scripts/PanelExample.js
+++ src/ExpandPanel/Scripts/PanelExample.js
@@ -1,7 +1,8 @@
 ﻿var Panel = {
     Expand: function(teamId) {
-        var url = "users?TeamId=" + teamId;
-        var team = $("#Team" + teamId);
+        var team = $("#Team" + teamId),
+            url = team.data("request-url");
+
         $.get(url, function(data) {
             team.html(data);
             team.slideToggle("slow", function() {});
